8408f67a3864b201f83efc13f7c722b76e9d6ff6
Ayush <ayushjadaun6@gmail.com>
2026-03-31 12:30:40 +0530

    chore(core): scaffold core package with tsup, vitest, zod

---
+++ packages/core/src/index.ts
@@ -0,0 +1,11 @@
+/**
+ * @i18n-platform/core
+ *
+ * Shared types, interfaces, adapter contracts, and validation schemas
+ * for the i18n automation platform.
+ *
+ * @packageDocumentation
+ */
+
+// Types will be exported here as they are created
+export {};
